5e3755c5947b36c0628875a91f6d181d8f980517
HaykalRadiandra <rthaykal@gmail.com>
2026-04-28 13:43:31 +0700

    update: fix image path navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,7 +31,7 @@ const Navbar = ({ isDarkMode, toggleDarkMode }) => {
       <div className="container mx-auto px-6 flex justify-between items-center">
         <a href="#" className="text-2xl font-bold flex items-center">
           <img
-            src="/public/env.webp"
+            src="/envteh/env.webp"
             alt="Logo Environment Technology"
             className="h-10 md:h-12 md:mr-2"
           />
